104942d5c086220877dbb846f1a48a7b92caca77
wangabeng <wangabeng@163.com>
2019-08-26 17:25:28 +0800

    update

---
+++ src/components/NotFound/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="not-found">
+    未找到
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  },
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "common/sass/variable.scss";
+.hello {
+  h1 {
+    color: $color-sub-red;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Index from '@/components/Index/Index'
 import UserCenter from '@/components/UserCenter/UserCenter'
 import HelloWorld from '@/components/HelloWorld/HelloWorld' // @别名 src
+import NotFound from '@/components/NotFound/NotFound'
 // import ItemCenter from '@/components/item-center/ItemCenter'
 // import Cart from '@/components/cart/Cart'
 // import Login from '@/components/login/Login'
@@ -11,7 +12,8 @@ import HelloWorld from '@/components/HelloWorld/HelloWorld' // @别名 src
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
+  // 如果开启history模式 会出现如果二级路由找不到 无法跳转到404页面的错误 此时需要后端配合 重定向到index.html页
+  // mode: 'history',
   routes: [
     {
       path: '/',
@@ -36,19 +38,40 @@ const router = new Router({
         requireAuth: true // 在需要登录的路由的meta中添加响应的权限标识
       }
     },
+    {
+      path: '*', //全不匹配的情况下，返回404，路由按顺序从上到下，依次匹配。最后一个*能匹配全部，
+      component: NotFound,
+    },
+    {
+      path: '*/*', //全不匹配的情况下，返回404，路由按顺序从上到下，依次匹配。最后一个*能匹配全部，
+      component: NotFound,
+    },
   ],
 
 })
 
-router.beforeEach((to, from, next) => {
-  console.log("路由TO：", to, "路由FROM：", from, "路由Next：", next);
-  console.log(router);
-  next();
-})
+// router.beforeEach((to, from, next) => {
+  /*console.log("路由TO：", to, "路由FROM：", from, "路由Next：", next);
+  console.log(router);*/
+  // 判断将要跳转的路由是否需要鉴权
+  /*console.log(to.matched.some(record => record.meta.requireAuth));
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    // 判断本地cookie是否包含sessionId字段
+    // 
+  }
+  next();*/
+  /*if (to.matched.length === 0) { //匹配前往的路由不存在
+    from.name ? next({
+      name: from.name
+    }) : next('/404'); //判断此跳转路由的来源路由是否存在，存在的情况跳转到来源路由，否则跳转到404页面
+  } else {
+    next(); //如果匹配到正确跳转
+  }*/
+// })
 
-router.afterEach((to, from) => {
+/*router.afterEach((to, from) => {
   console.log("后置守卫TO：", to, "后置守卫FROM：", from);
-})
+})*/
 
 // export default new Router({
 export default router
